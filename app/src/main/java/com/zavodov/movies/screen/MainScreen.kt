5d8f16814ff33f4c4b7e2ea734a7d12b23296e70
vm.zavodov <vladislav.zavodov@bsc-ideas.com>
2022-08-22 10:20:37 +0300

    Добавил верстку списка и переход на экран деталей.

--- app/src/main/java/com/zavodov/movies/screen/MainScreen.kt
+++ app/src/main/java/com/zavodov/movies/screen/MainScreen.kt
@@ -1,46 +1,94 @@
 package com.zavodov.movies.screen
 
-import android.util.Log
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import coil.compose.rememberImagePainter
 import com.zavodov.movies.MainViewModel
 import com.zavodov.movies.data.model.Movies
+import com.zavodov.movies.navigation.Screens
+import com.zavodov.movies.utils.Constants
+import okhttp3.internal.immutableListOf
 
 /**
  * Created by Zavodov on 21.08.2022
  */
 @Composable
 fun MainScreen(navController: NavHostController, viewModel: MainViewModel){
-	val allMovies = viewModel.allMovies.observeAsState(listOf()).value
-	allMovies.forEach {
-		Log.d("Zavodov", " MainScreen: ${it.id} ${it.name} ")
-	}
+	val allMovies = viewModel.allMovies.observeAsState(immutableListOf()).value
 	Surface(
 		modifier = Modifier.fillMaxSize()
 	) {
-		LazyColumn{
-			items(allMovies) {
-				MovieItem(item = it)
+		LazyColumn(
+			modifier = Modifier.padding(10.dp)
+		){
+			items(allMovies.take(30)) {
+				MovieItem(item = it, navController = navController)
 			}
 		}
 	}
 }
 
 @Composable
-fun MovieItem(item: Movies) {
-	Row(
-		modifier = Modifier.fillMaxSize()
+fun MovieItem(item: Movies, navController: NavHostController) {
+	Card(
+		elevation = 4.dp,
+		modifier = Modifier
+			.padding(top = 8.dp)
+			.clickable {
+				navController.navigate(Screens.Details.route + "/${item.id}")
+			}
 	) {
-		Text(text = item.id.toString())
-		Text(text = item.name)
+		Row(
+			modifier = Modifier
+				.fillMaxWidth()
+				.padding(vertical = 16.dp)
+		) {
+			Image(
+				painter = rememberImagePainter(item.image.medium),
+				contentDescription = null,
+				modifier = Modifier.size(120.dp)
+			)
+			Column {
+				Text(
+					text = item.name,
+					fontSize = 24.sp,
+					fontWeight = FontWeight.Bold)
+				Row {
+					Text(
+						text = "Rating: ",
+						fontWeight = FontWeight.Bold)
+					Text(
+						text = item.rating.average.toString())
+				}
+				Row {
+					Text(
+						text = "Genre: ",
+						fontWeight = FontWeight.Bold)
+					item.genres.take(2).forEach { Text(text = " $it ") }
+				}
+				Row {
+					Text(
+						text = "Premiered: ",
+						fontWeight = FontWeight.Bold)
+					Text(
+						text = item.premiered)
+				}
+			}
+
+		}
+
 	}
 }
